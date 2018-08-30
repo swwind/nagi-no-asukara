df0f40915d6244c00800965be9f0c2d3e1e0eec3
swwind <swwind233@gmail.com>
2018-08-30 21:01:40 +0800

    update

--- README.md
+++ README.md
@@ -14,18 +14,29 @@ Then start your server.
 
 ## Usage
 
-`/summon-sea-master` Summon a sea master here(op needed).
+`/summon-sea-master` Summon a sea master here. (op)
 
-Give him 5 puffer fishes will give you 100000s of water-breathing effect as rewards.
+Give him 5 puffer fishes will give you a permanent water-breathing effect as rewards.
 
-Since then you can live in the ocean! XD
-
-But instead, you would not able to stay without water for too long(actually 6 mins).
+However, you will not be able to stay without water for too long(actually 6 mins).
 
 Good Luck & Have Fun!
 
 ## Contribution
 
-<!-- TODO -->
+Welcome pull requests!
 
+### Import
+
+Clone this repo to local.
+
+Open eclipse > **File** > **Open Projects From File System...** > Enter your cloned path > **Finish**.
+
+Right click the project > **Properties** > **Java Build Path** > **Libraries** > **Add External JARs...** > Select your `spigot-1.13.1.jar` > **Apply and Close**.
+
+### Export
+
+Right click the project > **Export...** > **Java/JAR file** > Select `src/` and `plugin.yml`, set name to `NagiNoAsukara.jar` > **Finish**.
+
+Then start your server to play.
 
--- src/me/swwind/plugin/Main.java
+++ src/me/swwind/plugin/Main.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Server;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
@@ -22,6 +25,7 @@ import org.bukkit.entity.Villager.Profession;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
@@ -30,26 +34,36 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Main extends JavaPlugin implements Listener, CommandExecutor {
 
-	private FileConfiguration config = getConfig();
-
-	private HashMap<String, BossBar> bsr = new HashMap<>();
+	private FileConfiguration config;
+	private HashMap<String, BossBar> bsr;
+	private Server server;
+	private List<String> wetDeath;
+	private List<String> asking;
 
 	private final String CONFIG_NAME = "oceanman";
 	
-	private Server server;
-	
 	private BossBar createWaterBar(Player player) {
+
 		BossBar bossbar = server.createBossBar("Water Remains", BarColor.BLUE, BarStyle.SEGMENTED_6);
 		bossbar.setVisible(true);
 		bossbar.addPlayer(player);
+		
 		return bossbar;
 	}
 
 	@Override
 	public void onEnable() {
+
 		server = getServer();
+		config = getConfig();
+		bsr = new HashMap<>();
+		wetDeath = new ArrayList<>();
+		asking = new ArrayList<>();
+		
 		config.addDefault(CONFIG_NAME, new ArrayList<String>());
 		server.getPluginManager().registerEvents(this, this);
 		getCommand("summon-sea-master").setExecutor(this);
@@ -57,18 +71,25 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
 		// game interval
 		final int TIMES_PRE_SECOND = 4;
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+
 			@Override
 			public void run() {
+
 				List<Player> onlines = new ArrayList<>(getServer().getOnlinePlayers());
 
 				List<String> list = config.getStringList(CONFIG_NAME);
 
-				for (int i = 0; i < onlines.size(); ++i) {
+				for (int i = 0; i < onlines.size(); ++ i) {
 					
 					Player player = onlines.get(i);
 					String name = player.getName();
 					
 					if (list.contains(name)) {
+						
+						if (player.getGameMode() == GameMode.CREATIVE ||
+							player.getGameMode() == GameMode.SPECTATOR) {
+							return;
+						}
 
 						if (!bsr.containsKey(name)) {
 							bsr.put(name, createWaterBar(player));
@@ -76,11 +97,45 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
 						
 						BossBar bossbar = bsr.get(player.getName());
 						double grs = bossbar.getProgress();
+						
+						Block block = player.getLocation().getBlock();
 
-						if (player.getLocation().getBlock().isLiquid()) {
+						if (
+							// in lava
+							block.getType() == Material.LAVA ||
+								
+							// on fire
+							player.getFireTicks() > 0
+								
+							) {
+								
+							// not good for afterbirth
+							grs -= 1.0 / 5 / TIMES_PRE_SECOND;
+						
+						} else if (
+							// in a water log
+							block.getBlockData() instanceof Waterlogged ||
+								
+							// in water
+							block.getType() == Material.WATER ||
+								
+							// in sea grass
+							block.getType() == Material.KELP_PLANT ||
+							block.getType() == Material.SEAGRASS ||
+							block.getType() == Material.TALL_SEAGRASS ||
+								
+							// in rain
+							(player.getWorld().hasStorm() && player.getLocation().getBlockY() >= player.getWorld().getHighestBlockYAt(player.getLocation()))
+								
+							) {
+								
 							grs += 1.0 / 20 / TIMES_PRE_SECOND;
+							
 						} else {
+							
+							// on land
 							grs -= 1.0 / 360 / TIMES_PRE_SECOND;
+						
 						}
 						
 						grs = Math.min(grs, 1);
@@ -88,12 +143,21 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
 						bossbar.setVisible(grs < 1);
 						bossbar.setProgress(grs);
 						
+						int health = getHeath(grs);
+						
+						if (health == 0) {
+							wetDeath.add(name);
+							player.setHealth(0);
+						}
+						player.setHealthScale(health * 2);
+						
 						if (!player.hasPotionEffect(PotionEffectType.WATER_BREATHING)) {
 							giveAfterbirth(player);
 						}
 					}
 				}
 			}
+
 		}, 5, 20 / TIMES_PRE_SECOND);
 	}
 
@@ -102,8 +166,24 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
 		// TODO nothing
 	}
 
+	private int getHeath(double grs) {
+		int time = (int) (grs * 360);
+		if (time >= 180) return 10;
+		if (time >= 120) return 9;
+		if (time >= 90)  return 8;
+		if (time >= 60)  return 7;
+		if (time >= 50)  return 6;
+		if (time >= 40)  return 5;
+		if (time >= 30)  return 4;
+		if (time >= 20)  return 3;
+		if (time >= 10)  return 2;
+		if (time >= 1)   return 1;
+		return 0;
+	}
+
 	private void giveAfterbirth(Player player) {
-		player.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 100000, 1));
+
+		player.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 100000, 0));
 	}
 
 	@Override
@@ -135,7 +215,9 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
 	}
 
 	@EventHandler
-	public void onAskForAfterbirth(PlayerInteractEntityEvent e) {
+	public void onAskSeaMaster(PlayerInteractEntityEvent e) {
+		
+		e.setCancelled(true);
 
 		Player p = e.getPlayer();
 
@@ -144,50 +226,110 @@ public class Main extends JavaPlugin implements Listener, CommandExecutor {
 			Villager v = (Villager) e.getRightClicked();
 
 			if (v.getCustomName().equalsIgnoreCase("海神")) {
-
-				Inventory inventory = p.getInventory();
-
-				List<String> list = config.getStringList(CONFIG_NAME);
-
-				if (list.contains(p.getName())) {
-					p.sendMessage("你已经有胞衣了");
+				
+				if (asking.contains(p.getName())) {
 					return;
 				}
-
-				if (!inventory.containsAtLeast(new ItemStack(Material.PUFFERFISH), 5)) {
-					p.sendMessage("<海神> 抱歉，我需要 5 个河豚才能帮你办事");
-					return;
-				}
-
-				list.add(p.getName());
-				config.set(CONFIG_NAME, list);
-				inventory.removeItem(new ItemStack(Material.PUFFERFISH, 5));
-				giveAfterbirth(p);
-				p.sendMessage("<海神> 感谢你的河豚，我已经激活了你的胞衣");
-
-				saveConfig();
-
+				
+				p.sendMessage("<海神> 你想要我做什么？");
+				p.sendMessage("1) 获取胞衣（需要 5 个河豚）");
+				p.sendMessage("2) 换取 16 个海泡菜（需要 64 个海带）");
+				asking.add(p.getName());
 			}
 		}
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		Player p = event.getPlayer();
+		String name = p.getName();
+		
+		if (asking.contains(name)) {
+			asking.remove(name);
+			event.setCancelled(true);
+			
+			String message = event.getMessage();
+			switch (message) {
+			case "1": // ask for after birth
+				handleAskForAfterbirth(p);
+				break;
+			case "2": // ask for sea pickles
+				handleAskForSeaPickles(p);
+				break;
+			default:
+				p.sendMessage(ChatColor.RED + "未知指令");
+			}
+		}
+	}
+	
+	public void handleAskForAfterbirth(Player p) {
+		Inventory inventory = p.getInventory();
+		List<String> list = config.getStringList(CONFIG_NAME);
+
+		if (list.contains(p.getName())) {
+			p.sendMessage("<海神> 你已经有胞衣了");
+			return;
+		}
+
+		if (!inventory.containsAtLeast(new ItemStack(Material.PUFFERFISH), 5)) {
+			p.sendMessage("<海神> 抱歉，我需要 5 个河豚才能帮你办事");
+			return;
+		}
+
+		list.add(p.getName());
+		config.set(CONFIG_NAME, list);
+		inventory.removeItem(new ItemStack(Material.PUFFERFISH, 5));
+		p.sendMessage("<海神> 感谢你的河豚，你可以在水下自由呼吸了");
+		
+		saveConfig();
+	}
+
+	private void handleAskForSeaPickles(Player p) {
+		Inventory inventory = p.getInventory();
+
+		if (!inventory.containsAtLeast(new ItemStack(Material.KELP), 64)) {
+			p.sendMessage("<海神> 我需要 64 个海带作为交换");
+			return;
+		}
+
+		inventory.removeItem(new ItemStack(Material.KELP, 64));
+		inventory.addItem(new ItemStack(Material.SEA_PICKLE, 16));
+		p.sendMessage("<海神> 感谢你的海带");
 	}
 
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent event) {
+
 		Player p = event.getEntity();
+		String name = p.getName();
+		
+		if (wetDeath.contains(name)) {
+			event.setDeathMessage(name + " 干死了");
+			wetDeath.remove(name);
+		}
 
 		List<String> list = config.getStringList(CONFIG_NAME);
-		if (list.remove(p.getName())) {
+		
+		if (list.remove(name)) {
 			config.set(CONFIG_NAME, list);
 			saveConfig();
 		}
-
-		bsr.remove(p.getName());
+		
+		if (bsr.containsKey(name)) {
+			BossBar b = bsr.get(name);
+			b.removeAll();
+			bsr.remove(name);
+		}
+		
+		p.setHealthScale(20);
 	}
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
+
 		Player p = event.getPlayer();
 		BossBar bossbar = bsr.get(p.getName());
+
 		if (bossbar != null) {
 			bossbar.addPlayer(p);
 		}
